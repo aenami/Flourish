522e72f3f65b62c3fb2c50d9cce0fb8fdb6de3a5
Julian <juliannandres05@gmail.com>
2026-05-28 18:34:57 -0500

    Se crea el endpoint para consumir los elementos

---
+++ server/src/controllers/elements.controller.ts
@@ -0,0 +1,31 @@
+import type { Request, Response} from 'express' // Importamos los tipos de datos para req/res
+import prisma from '../lib/prisma.js'
+
+export const Elements = async (req: Request, res: Response) => {
+    try {
+        // 1. Tomamos el id del usuario
+        const idUser = req.idUser
+
+        // 2. Traemos los elementos relacionadas al usuario junto con sus habitos respectivos
+        const elements = await prisma.elemento.findMany({
+            where: {
+                habito: {
+                    id_usuario_habito: idUser
+                }
+            }
+        })
+
+        res.status(200).json({
+            error: false,
+            data: elements
+        })
+
+    } catch (error) {
+        // Devolvemos la respuesta del error de nuestro modelo al frontend
+        console.log('Error al obtener todos los elementos ', error)
+        res.status(500).json({
+            error: true,
+            message: error
+        })
+    }
+}
---
+++ server/src/routes/elements.routes.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+// Importamos metodos de nuestro controlador
+import { Elements } from '../controllers/elements.controller.js';
+import { authenticateUser } from '../middlewares/auth.middleware.js';
+const router = express.Router()
+
+// Ruta para traer todos los habitos y la información mas relevante relacionada a estos
+router.get('/Elements', authenticateUser, Elements)
+
+
+
+export default router
+
